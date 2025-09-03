0197427aa5620eab79788b82f44982264ef570dd
Walter A <walter.arruda.alves@gmail.com>
2025-09-03 13:47:01 -0300

    chore: add sidekiq settings
    - Add sidekiq gem;
    - Add sidekiq initializer;
    - Add sidekiq and redis in docker-compose;

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+sidekiq_redis_config = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_redis_config
+end
